254c757e5f62c39822ae02b19dc95927b69daf55
James Czepiel <jaczepiel@gmail.com>
2021-05-08 22:25:48 -0400

    add marker to map when hovering over camp on index page

--- public/javascripts/indexPage.js
+++ public/javascripts/indexPage.js
@@ -1,13 +1,19 @@
 const allNumberedPaginationButtons = document.querySelectorAll("#numberedPaginationButton")
 const allWordedPaginationButtons = document.querySelectorAll("#wordedPaginationButton")
+const allCards = document.querySelectorAll("#indexpagecard")
+
+for (aCard of allCards) {
+    aCard.addEventListener('mouseenter', function (event) {
+        cardHovered(event)
+    })
+}
+
 
 var currentPageNumber = parseInt(currentPage)
 
 const arrayOfNumberedPaginationButtons = [...allNumberedPaginationButtons]
 
 const arrayOfAllCurrentPageButtonNumbers = arrayOfNumberedPaginationButtons.map(e => parseInt(e.innerText))
-
-console.log(arrayOfAllCurrentPageButtonNumbers)
 
 if (arrayOfAllCurrentPageButtonNumbers.some(el => el > (currentPageNumber + 2)) || arrayOfAllCurrentPageButtonNumbers.some(el => el < (currentPageNumber - 2))) {
     // If any button number is greater than current page + 2 or less than current page - 2, that means we need to update the button numbers and shift them over to center on the new current page number
@@ -32,4 +38,34 @@ for (aWordedPaginationButton of allWordedPaginationButtons) {
         aWordedPaginationButton.href = `/campgrounds?page=${currentPageNumber - 1}&limit=${numberPerPage}`
     }
 }
+var mapMarker
+function cardHovered(event) {
 
+    var title = ''
+    for (possibleTitle of event.target.getElementsByClassName('card-title')) {
+        title = possibleTitle.innerText
+    }
+
+    console.log('searching for inner text: ', title)
+
+    const hoveredCamp = parsedCamps.find(el => el.title === title);
+
+    console.log('hoveredCamp: ', hoveredCamp)
+
+
+    if (hoveredCamp) {
+        if (mapMarker) {
+            mapMarker.remove()
+        }
+
+        mapMarker = new mapboxgl.Marker()
+            .setLngLat(hoveredCamp.geometry.coordinates)
+            .setPopup(
+                new mapboxgl.Popup({ offset: 25 })
+                    .setHTML(
+                        `<h3>${hoveredCamp.title}</h3><p>${hoveredCamp.location}</p>`
+                    )
+            )
+            .addTo(map);
+    }
+}
